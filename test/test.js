8a0926f506137d595ad25704dd8c3534ccb7191c
Achanandhi-M <achanandhi.m@gmail.com>
2023-11-09 22:15:47 +0530

    pushed

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('GET /', () => {
       .get('/')
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.text).to.equal('Hello, World!');
+        expect(res.text).to.equal('Hello, Vidhyouth Oluga padi da');
         done();
       });
   });
